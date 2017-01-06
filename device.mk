1126bf2f64b9f1dd73d147dbbc3cfaf15ed6d2c4
wrapped <ddroidx@gmail.com>
2017-01-05 20:51:19 -0600

    pme: Lets not build ANT+ right now
    This reverts commit 7148757282a79fafc865529d9b185f01338e2e56.
    
    Change-Id: I06c74be23bebb651e9eb8fe1e6670cdd986f95d9
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- device.mk
+++ device.mk
@@ -78,15 +78,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/adblocker:system/bin/adblocker
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
