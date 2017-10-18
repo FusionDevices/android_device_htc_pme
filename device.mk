54ba8563034c0ae7061d365d9b959fa6d1e2f2da
Tab Fitts <tfitts1212@gmail.com>
2017-10-17 17:32:35 -0700

    [pme] Re-work releasetools add variants script.
    Change-Id: I8a878832623d19c7cb250f102404b3f9c05eb753

--- device.mk
+++ device.mk
@@ -213,6 +213,10 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.firmware_links.sh
 
+# Variant linking script
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/variants.sh:install/bin/variants.sh
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/synaptics_dsx.idc:system/usr/idc/synaptics_dsx.idc
