078605723716476297a7e98af9a439108a372906
Tab Fitts <tfitts1212@gmail.com>
2017-12-13 13:29:23 -0700

    [pme] Fix duplicate net packages
    Change-Id: Ib9a7ace73b5b91ea10a3ed156b8e8a7ce3d4b988

--- device.mk
+++ device.mk
@@ -274,11 +274,6 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     netutils-wrapper-1.0
 
-# Net
-PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0 \
-    netutils-wrapper-1.0
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
